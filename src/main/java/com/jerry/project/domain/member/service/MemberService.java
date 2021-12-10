e14dc72bef6addcc25cb4de45028bcf3a5617adf
phm3241 <parkhm3241@gmail.com>
2021-12-10 11:27:56 +0900

    commit 통합

--- src/main/java/com/jerry/project/domain/member/service/MemberService.java
+++ src/main/java/com/jerry/project/domain/member/service/MemberService.java
@@ -3,9 +3,10 @@ package com.jerry.project.domain.member.service;
 import com.jerry.project.domain.member.mapper.MemberMapper;
 import com.jerry.project.domain.member.model.entity.Member;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -13,15 +14,20 @@ public class MemberService {
     private final MemberMapper memberMapper;
 
     // db 확인 말고, 우선 id=pw 조건으로 로그인처리 구현
-    public Member login(String id, String pw){
+    public Member login(String id, String pw) throws Exception {
 
-        Member member = null;
-        if(id.equals(pw)){
-            member = new Member();
-            member.setId(id);
-            member.setPw(pw);
-            member.setName("이름");
-        }
+//        List<Member> memberList = memberMapper.getMembers();
+//        log.info("memberList : {}", memberList);
+        Member member = memberMapper.chekLogin(id, pw);
+        log.info("member : {}", member);
+
+//        Member member = null;
+//        if(id.equals(pw)){
+//            member = new Member();
+//            member.setId(id);
+//            member.setPw(pw);
+//            member.setName("이름");
+//        }
         return member;
     }
 
